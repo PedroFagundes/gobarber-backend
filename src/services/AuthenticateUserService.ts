372986de14fed86ecf92a9020884b81803d8221a
Pedro Fagundes <pedrofagundesb@gmail.com>
2020-04-26 16:01:22 -0300

    Creating JWT and sending it as authentication response

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw Error('Invalid email or password.');
     }
 
-    return { user };
+    const token = sign({}, 'bee460963374b2cca9ea3dfe6e0aecf6', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
